446f91402a5e88ba5768496409dce7e118770702
iqbalkimochi <71366986+Iqbal1007@users.noreply.github.com>
2020-09-17 04:05:15 +0700

    Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	
+	public static void main(String[] args){
+		System.out.println("Nama : muhammad iqbal");
+		System.out.println("Nim  : 6706200121");
+		System.out.println("kelas: D3IF 44-04");
+	}
+} 
